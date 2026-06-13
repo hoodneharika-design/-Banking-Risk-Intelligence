0683c3b468daffe0b0e0038758f5b38e8dcd41a6
Neharika2025 <neharikahood946@gmail.com>
2026-06-13 18:32:43 +0530

    Fix API key loading from Streamlit secrets

--- rag/agent.py
+++ rag/agent.py
@@ -2,6 +2,8 @@ import sys
 import os
 import numpy as np
 np.float_ = np.float64
+
+
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from langchain_groq import ChatGroq
